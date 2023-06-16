715f2783d1cc9efc4bd0917e74d648137b9909ef
Alexander <alek-dunaev@mail.ru>
2023-06-16 14:26:04 +0500

    Добавил решение задачи 1

---
+++ Solution/1.Multiply.py
@@ -0,0 +1,9 @@
+from operator import mul as mult_two
+
+print("Example")
+print(mult_two(10, 2))
+
+assert mult_two(3, 2) == 6
+assert mult_two(0, 1) == 0
+
+print("The first mission is done! Click 'Check' to earn cool rewards!")
