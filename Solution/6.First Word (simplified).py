dee29844bf66b6cecea388e2b0d5bc09be0ba8c1
Alexander <alek-dunaev@mail.ru>
2023-06-16 16:01:09 +0500

    Добавил решение задачи 5

---
+++ Solution/6.First Word (simplified).py
@@ -0,0 +1,17 @@
+def first_word(text: str) -> str:
+    # return text.split(' ')[0]
+    # first_word = lambda s: s.split()[0]
+    index = text.find(" ")
+    return text[:index] if index != -1 else text
+
+
+print("Example:")
+print(first_word("Hello world"))
+
+# These "asserts" are used for self-checking
+assert first_word("Hello world") == "Hello"
+assert first_word("a word") == "a"
+assert first_word("greeting from CheckiO Planet") == "greeting"
+assert first_word("hi") == "hi"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
