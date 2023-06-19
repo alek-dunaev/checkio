7ad9e8f9834fbed7e0aac7902abe47496699e37a
Alexander <alek-dunaev@mail.ru>
2023-06-19 13:14:15 +0500

    Добавил решение задачи 11

--- Solution/11.Acceptable Password IV.py
+++ Solution/11.Acceptable Password IV.py
@@ -1,10 +1,11 @@
 def is_acceptable_password(password: str) -> bool:
-    if 6 < len(password) < 9 and any(i.isdigit() for i in password) and not all(i.isdigit() for i in password):
-        return True
-    elif len(password) > 9:
-        return True
-    else:
-        return False
+    return (len(password) > 6 and not password.isdigit() and not password.isalpha()) or len(password) > 9
+    # if 6 < len(password) < 9 and any(i.isdigit() for i in password) and not all(i.isdigit() for i in password):
+    #     return True
+    # elif len(password) > 9:
+    #     return True
+    # else:
+    #     return False
 
 
 print("Example:")
