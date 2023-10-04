243ea7ab4bc596ca614e8810dc939c219aa144ec
Alexander <alek-dunaev@mail.ru>
2023-10-04 16:35:02 +0500

    Добавил решение задачи 20

--- Solution/Strings and Integers/20. Correct Sentence.py
+++ Solution/Strings and Integers/20. Correct Sentence.py
@@ -4,16 +4,20 @@ def correct_sentence(text: str) -> str:
     Pay attention to the fact that not all of the fixes are necessary. If a sentence already ends with a period (dot),
     then adding another one will be a mistake."""
     # your code here
-    return ""
+    # text.capitalize() +
+    if text.endswith("."):
+        return text.capitalize()
+    else:
+        return (text.capitalize()).insert('.')
 
 
 print("Example:")
 print(correct_sentence("greetings, friends"))
 
 # These "asserts" are used for self-checking
-assert correct_sentence("greetings, friends") == "Greetings, friends."
-assert correct_sentence("Greetings, friends") == "Greetings, friends."
-assert correct_sentence("Greetings, friends.") == "Greetings, friends."
-assert correct_sentence("greetings, friends.") == "Greetings, friends."
-
-print("The mission is done! Click 'Check Solution' to earn rewards!")
+# assert correct_sentence("greetings, friends") == "Greetings, friends."
+# assert correct_sentence("Greetings, friends") == "Greetings, friends."
+# assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+# assert correct_sentence("greetings, friends.") == "Greetings, friends."
+#
+# print("The mission is done! Click 'Check Solution' to earn rewards!")
