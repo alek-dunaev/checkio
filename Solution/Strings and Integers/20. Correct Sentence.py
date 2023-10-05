14c1298977651dab51ffda6fadb1501cbd2abfeb
Alexander <alek-dunaev@mail.ru>
2023-10-05 10:00:50 +0500

    Добавил решение задачи 20

--- Solution/Strings and Integers/20. Correct Sentence.py
+++ Solution/Strings and Integers/20. Correct Sentence.py
@@ -6,6 +6,14 @@ def correct_sentence(text: str) -> str:
     # your code here
     # text[0].upper() + text[1:] + ("." if text[-1] != "." else "")
     # correct_sentence = lambda t: t[0].upper() + t[1:] + '.'*(t[-1] != '.')
+    #     text = np.array(list(text), dtype=str)
+    #     dot_at_the_end = text[-1] in '.'
+    #     capital_at_start = text[0] in ascii_uppercase
+    #     if not capital_at_start:
+    #         text[0] = str(text[0]).upper()
+    #     if not dot_at_the_end:
+    #         text = np.append(text,'.')
+    #     return "".join(text)
     if text.endswith("."):
         return (text[0:text.find(" ")]).title() + text[text.find(" "):]
     else:
